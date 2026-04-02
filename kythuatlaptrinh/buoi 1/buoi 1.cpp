a9a365b0ee46429b9b473626d3a4057072f2e8b7
acer <acer@LAPTOP-RF4GEUJL>
2026-04-02 13:23:42 +0700

    baitap1

--- kythuatlaptrinh/buoi 1/buoi 1.cpp
+++ kythuatlaptrinh/buoi 1/buoi 1.cpp
@@ -1,175 +1,184 @@
-﻿
+﻿#include <iostream>
 #include <vector>
-#include <iostream>
-#include <string> // Cần thêm thư viện này để dùng kiểu dữ liệu string
-#include <list>
+#include <string>
 #include <fstream>
-
 using namespace std;
 
-struct person {
-    int id, age;
-    string name, hometown;
-    friend istream& operator>>(istream& in, person& p) {
-        cout << " enter person information :" << endl;
-        cout << "\t+ Id:";
-        in >> p.id;
-        cout << "x\t+Name:";
-        in.ignore();
-        getline(in, p.name);
-        cout << "\t+ Age:";
-        in >> p.age;
-        cout << "\t+ Home town:";
-        in.ignore();
-        getline(in, p.hometown);
-        return in;
-    }
-    friend ostream operator < (ostream& out, person p) {
-        out << " Person information :" << endl;
-        out << " \t+ Id:" << p.id << endl;
-        out << " \t+ Name:" << p.name << endl;
-        out << " \t+ Age:" << p.age << endl;
-        out << " \t+ hometown:" << p.hometown << endl;
-        return out;
-    }
-
-
+struct Person {
+	int id, age;
+	string name, hometown;
+	friend istream& operator>>(istream& in, Person& p) {
+		cout << "Enter person information:" << endl;
+		cout << "\t+ Id: ";
+		in >> p.id;
+		cout << "\t+ Name: ";
+		in.ignore();
+		getline(in, p.name);
+		cout << "\t+ Age: ";
+		in >> p.age;
+		cout << "\t+ Home town: ";
+		in.ignore();
+		getline(in, p.hometown);
+		return in;
+	}
+	friend ostream& operator<<(ostream& out, Person p) {
+		out << "Person information:" << endl;
+		out << "\t+ Id: " << p.id << endl;
+		out << "\t+ Name: " << p.name << endl;
+		out << "\t+ Age: " << p.age << endl;
+		out << "\t+ Home town: " << p.hometown << endl;
+		return out;
+	}
 };
 
-void show(vector<person > p) {
-    if (p.size() == 0)
-        cout << "An empty list " << endl;
-    else {
-        for (int i = p.size() - 1; i >= 0; i--) {
-            cout << " \t+ Id" << p[i].id << endl;
-            cout << " \t+ Name" << p[i].name << endl;
-            cout << " \t+ Age" << p[i].age << endl;
-            cout << " \t+ hometown" << p[i].hometown << endl;
-        }
-    }
+void Show(vector<Person> p) {
+	if (p.size() == 0)
+		cout << "An empty list" << endl;
+	else {
+		for (int i = p.size() - 1; i >= 0; i--) {
+			cout << "Person information:" << endl;
+			cout << "\t+ Id: " << p[i].id << endl;
+			cout << "\t+ Name: " << p[i].name << endl;
+			cout << "\t+ Age: " << p[i].age << endl;
+			cout << "\t+ Home town: " << p[i].hometown << endl;
+		}
+	}
 }
-bool remove(vector<person >& p, int id) {
-    for (int i = 0; i < p.size(); i++);{
-        if (p[i].id == id) {
-        p.erase(p.begin() + i);
-        return true;
-      }
-    }
-    return false;
+bool Remove(vector<Person>& p, int id) {
+	for (int i = 0; i < p.size(); i++) {
+		if (p[i].id == id) {
+			p.erase(p.begin() + i);
+			return true;
+		}
+	}
+	return false;
 }
-void find(vector <person >p, string name) {
-    bool found = false;
-    for (int i = p.size() - 1; i >= 0; i--) {
-        if (p[i].name == name) {
-            found = true;
-            cout << p[i];
-
-        }
-    }
-    if (!found);
+void Find(vector<Person> p, string name) {
+	bool found = false;
+	for (int i = p.size() - 1; i >= 0; i--) {
+		if (p[i].name == name) {
+			found = true;
+			cout << p[i];
+		}
+	}
+	if (!found)
+		cout << "No person is found with name: " << name << endl;
 }
-void export (vector <person > p, string filename) {
-    ofstream out(filename , ios ::binary);
-    if (!out.is_open()) {
-        cout << "can't open file:" << filename << endl;
-        return;
+void Export(vector<Person> p, string filename) {
+	ofstream out(filename, ios::binary);
+	if (!out.is_open()) {
+		cout << "Can't open file: " << filename << endl;
+		return;
+	}
+	for (int i = 0; i < p.size(); i++) {
+		out.write(reinterpret_cast<const char*>(&p[i].id), sizeof(p[i].id));
 
-    }
-    for (int i = 0; i < p.size(); i++) {
-        out.close();
-        cout << " Export successfully to file: " << filename << endl;
-    }
+		size_t namelength = p[i].name.size();
+		out.write(reinterpret_cast<const char*>(&namelength), sizeof(namelength));
+		out.write(p[i].name.c_str(), namelength);
 
+		out.write(reinterpret_cast<const char*>(&p[i].age), sizeof(p[i].age));
+
+		size_t hometownlength = p[i].hometown.size();
+		out.write(reinterpret_cast<const char*>(&hometownlength), sizeof(hometownlength));
+		out.write(p[i].hometown.c_str(), hometownlength);
+	}
+	out.close();
+	cout << "Export successfully to file: " << filename << endl;
 }
-void import(vector <person > p, string filename) {
-    ifstream in(filename);
-    if (!in.is_open()) {
-        cout << " can 't open file :" << filename << endl;
-        return;
-    }
-    p.clear();
-    while (!in.eof()) {
-        person a;
-        a.id = 0;
-        in >> a.id;
-        in.ignore();
-        getline(in, a.filename);
-        in >> a.age;
-        in.ignore();
-        getline(in, a.hometown);
-        if (a.id = 0)
-            break;
-        p.push_back(a);
+void Import(vector<Person>& p, string filename) {
+	ifstream in(filename, ios::binary);
+	if (!in.is_open()) {
+		cout << "Can't open file: " << filename << endl;
+		return;
+	}
+	p.clear();
 
-    }
-    in.close();
-    cout << "import  successfully file:" << filename << endl;
+	Person a;
+	while (in.peek() != EOF) {
+		in.read(reinterpret_cast<char*>(&a.id), sizeof(a.id));
+
+		size_t nameLength;
+		in.read(reinterpret_cast<char*>(&nameLength), sizeof(nameLength));
+		a.name.resize(nameLength);
+		in.read(&a.name[0], nameLength);
+
+		in.read(reinterpret_cast<char*>(&a.age), sizeof(a.age));
+
+		size_t hometownLength;
+		in.read(reinterpret_cast<char*>(&hometownLength), sizeof(hometownLength));
+		a.hometown.resize(hometownLength);
+		in.read(&a.hometown[0], hometownLength);
+
+		p.push_back(a);
+	}
+	in.close();
+	cout << "Import successfully from file: " << filename << endl;
 }
 
-void main() {
-    vector<person> list;
-    
-    do {
-        system("cls");
-        cout << "           HUMAN RESOURCE MANAGEMENT          " << endl;
-        cout << "1. Show person list" << endl;
-        cout << "2. ADD Person list" << endl;
-        cout << "3. Remove a person by Id" << endl;
-        cout << "4. Find people by name" << endl;
-        cout << "5.export to file" << endl;
-        cout << "6.import to file" << endl;
-        cout << "0. Quit" << endl;
-        int cmd;
-        cin >> cmd;
-        switch (cmd) {
-        case 1: {
-            break;
-        }
-        case 2: {
-            person a;
-            cin >> a;
-            list.push_back(a);
-            break;
-        }
-        case 3: {
-            int removedId;
-            cout << "enter Id to remove:";
-            cin >> removedId;
-            bool res = remove (list, removedId);
-            if (res)
-                cout << "remove succesfully" << endl;
-            else
-                cout << "Id not found. Try aganin" << endl;
-            break;
-        }
-        case 4: {
-            string name;
-            cout << " enter name to find:";
-            cin.ignore();
-            getline(cin, name);
-            find(list, name);
-
-            break;
-        }
-        case 5: {
-            export (list, "25th2 dla");
-            break;
-        }
-        case 6: {
-            import (list, "25th2 dla");
-            break;
-        }
-        case 0: {
-            break;
-        }
-        default: {
-            cout << "Your command isn't found. Try again!" << endl;
-        }
-        }
-        cout << "Press Enter to coutinue.....";
-        cin.ignore();
-        cin.get();
-
-    } while (true);
-
+int main() {
+	vector<Person> list;
+	do {
+		system("cls");
+		cout << "---- HUMAN RESOURCE MANAGEMENT ----" << endl;
+		cout << "1. Show person list" << endl;
+		cout << "2. Add a person" << endl;
+		cout << "3. Remove a person by id" << endl;
+		cout << "4. Find people by name" << endl;
+		cout << "5. Export to file" << endl;
+		cout << "6. Import from file" << endl;
+		cout << "0. Quit" << endl;
+		cout << "-----------------------------------" << endl;
+		cout << "Your command: ";
+		int cmd;
+		cin >> cmd;
+		switch (cmd) {
+		case 1: {
+			Show(list);
+			break;
+		}
+		case 2: {
+			Person a;
+			cin >> a;
+			list.push_back(a);
+			break;
+		}
+		case 3: {
+			int removeId;
+			cout << "Enter id to remove: ";
+			cin >> removeId;
+			bool res = Remove(list, removeId);
+			if (res)
+				cout << "Remove successfully" << endl;
+			else
+				cout << "Id not found. Try again" << endl;
+			break;
+		}
+		case 4: {
+			string name;
+			cout << "Enter name to find: ";
+			cin.ignore();
+			getline(cin, name);
+			Find(list, name);
+			break;
+		}
+		case 5: {
+			Export(list, "25TH2.dla");
+			break;
+		}
+		case 6: {
+			Import(list, "25TH2.dla");
+			break;
+		}
+		case 0: {
+			return 0;
+		}
+		default: {
+			cout << "Your command isn't found. Try again" << endl;
+		}
+		}
+		cout << "Press Enter to continue...";
+		cin.ignore();
+		cin.get();
+	} while (true);
 }
